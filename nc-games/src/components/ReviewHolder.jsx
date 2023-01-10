8a2350ec1a83ff79126535ff670cdaab27d07722
karlrivkpmg <karl.rivett@kpmg.co.uk>
2023-01-10 10:37:46 +0000

    added loading functionality

--- nc-games/src/components/ReviewHolder.jsx
+++ nc-games/src/components/ReviewHolder.jsx
@@ -4,13 +4,22 @@ import { ReviewCard } from './ReviewCard';
 
 export const ReviewHolder = () =>{
     const [reviews, setReviews] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(()=>{
+        setIsLoading(true);
         getReviews()
         .then((data)=>{
+            setIsLoading(false);
             setReviews(data);
         })
     }, [])
+
+    if(isLoading){
+        return(
+            <p>Loading...</p>
+        )
+    }
 
     return (
         <div>
